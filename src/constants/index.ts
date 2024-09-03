1fb5db6bae60b3cef824f806007d7825762fe3eb
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-03 19:55:24 +0700

    feat: add new constant for response message

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,25 @@
 const ROUTES = {
   BASE: '/api',
   PRODUCTS: '/products',
-  PRODUCT: '/product/:id'
+  PRODUCT: '/product/:id',
+  BRANDS: 'brands'
 }
 
-const ERROR_MESSAGE = {
-  NOT_FOUND: 'Not Found'
+const MESSAGE = {
+  ERROR: {
+    NOT_FOUND: 'Not Found',
+  },
+  SUCCESS: {
+    GET_PRODUCTS: 'Successfully get products',
+    GET_PRODUCT: 'Successfully get product',
+    ADD_PRODUCT: 'Successfully adding product',
+    DELETED_PRODUCT: 'Successfully deleted product',
+    UPDATED_PRODUCT: 'Successfully update product',
+    ADD_BRAND: 'Successfully adding brand'
+  }
 }
 
 export {
   ROUTES,
-  ERROR_MESSAGE
+  MESSAGE
 }
