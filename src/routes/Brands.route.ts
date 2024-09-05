3c086ef91582a296cb17a45818d11afaf423a9ef
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-05 21:03:38 +0700

    feat: add route and service for brands (get all, get one, update brand, delete brand)

--- src/routes/Brands.route.ts
+++ src/routes/Brands.route.ts
@@ -1,11 +1,32 @@
 import { Hono } from "hono";
 
-import { Brands } from "@prisma/client";
+import { Brand, Brands } from "@prisma/client";
 
 import { MESSAGE, ROUTES } from "../constants";
-import { addBrands } from "../services/Brands.service";
+import { addBrands, deleteBrandById, getBrandById, getBrands, updateBrandById } from "../services/Brands.service";
 
 const brand = new Hono();
+
+brand.get(ROUTES.BRANDS, async (c) => {
+  const brands = await getBrands();
+
+  return c.json({
+    status: true,
+    message: MESSAGE.SUCCESS.GET_BRANDS,
+    data: brands
+  }, 200)
+});
+
+brand.get(ROUTES.BRAND, async (c) => {
+  const id = c.req.param('id')
+  const brand = await getBrandById(id)
+
+  return c.json({
+    status: true,
+    message: MESSAGE.SUCCESS.GET_BRAND,
+    data: brand
+  }, 200)
+})
 
 brand.post(ROUTES.BRANDS, async (c) => {
   let body: Partial<Brands>
@@ -25,6 +46,38 @@ brand.post(ROUTES.BRANDS, async (c) => {
     message: MESSAGE.SUCCESS.ADD_BRAND,
     data: newBrand
   }, 201)
+})
+
+brand.delete(ROUTES.BRAND, async (c) => {
+  const {id} = c.req.param()
+  const deletedBrand = await deleteBrandById(id)
+
+  return c.json({
+    status: true,
+    message: MESSAGE.SUCCESS.DELETED_PRODUCT,
+    data: deletedBrand
+  })
+})
+
+brand.patch(ROUTES.BRAND, async (c) => {
+  const id = c.req.param('id')
+  let body: Partial<Brand>
+  const contentType = c.req.header('Content-Type')
+
+  if (contentType === 'application/json') {
+    body = await c.req.json()
+  } else {
+    body = await c.req.parseBody<Partial<Brand>>()
+  }
+
+  const {name} = body
+  const updateBrand = await updateBrandById({id, name})
+
+  return c.json({
+    status: true,
+    message: MESSAGE.SUCCESS.UPDATED_BRAND,
+    data: updateBrand
+  })
 })
 
 export { brand };
--- src/services/Brands.service.ts
+++ src/services/Brands.service.ts
@@ -2,6 +2,22 @@ import { PrismaClient, Brand } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
+export async function getBrands(): Brand[] {
+  const brands = await prisma.brand.findMany()
+
+  return brands;
+}
+
+export async function getBrandById(id: string): Brand {
+  const brand = await prisma.brand.findFirst({
+    where: {
+      id: +id
+    }
+  })
+
+  return brand
+}
+
 export async function addBrands(data: Partial<Brand>) {
   const {name} = data;
 
@@ -12,4 +28,32 @@ export async function addBrands(data: Partial<Brand>) {
   })
 
   return newBrand
+}
+
+export async function deleteBrandById(id: string) {
+  const deletedBrand = await prisma.brand.delete({
+    where: {
+      id: +id
+    }
+  })
+
+  return deletedBrand
+}
+
+export async function updateBrandById(data: Partial<Brand>) {
+  const {
+    id,
+    name,
+  } = data
+
+  const updatedBrand = await prisma.brand.update({
+    where: {
+      id: +id
+    },
+    data: {
+      name: name || undefined,
+    }
+  })
+
+  return updatedBrand
 }
