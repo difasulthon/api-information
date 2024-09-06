d6ac2f8ec549ee24c68a2e3bd3f89f58f6c69674
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-06 22:37:16 +0700

    feat: set up doc swagger with add new routing

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import { Hono } from 'hono'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui';
 // import { cors } from "hono/middleware"
 
 import { MESSAGE, ROUTES } from './constants';
 import { product } from './routes/Product.route';
 import { brand } from './routes/Brands.route';
 
-const api = new Hono();
+const api = new OpenAPIHono({strict: false});
 
 // api.use("/api/*", cors());
 
 api.route(ROUTES.BASE, product);
 api.route(ROUTES.BASE, brand);
 api.notFound((c) => c.json({ message: MESSAGE.ERROR.NOT_FOUND }, 404));
+
+api.doc('/doc', {
+  openapi: '3.1.0',
+  info: {
+    version: '1.0.0',
+    title: 'Shoes Product API',
+    description: 'This is the REST API for football shoe',
+  },
+})
+api.get(ROUTES.BASE, swaggerUI({ url: '/doc' }));
 
 export default {
   port: 80,
