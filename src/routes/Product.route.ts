c8fe79df9511ec0324d3103b13d0fea0ce28aed5
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-06 22:39:54 +0700

    feat: update product routes with swagger docs

--- src/routes/Product.route.ts
+++ src/routes/Product.route.ts
@@ -1,89 +1,215 @@
 import { Hono } from "hono";
+import { OpenAPIHono } from '@hono/zod-openapi'
 
 import { Product } from "@prisma/client";
 
-import { ROUTES, MESSAGE } from '../constants';
+import { ROUTES, MESSAGE, TAGS } from '../constants';
 import { 
   addProduct, deleteProductById, getProductById, getProducts, 
   updateProductById
 } from "../services/Products.service";
+import { bodyAddProductSchema, bodyUpdateProductSchema, paramProductByIdSchema, queryProductSchema } from "../schemas/product.schema";
 
-const product = new Hono();
+const product = new OpenAPIHono()
 
-product.get(ROUTES.PRODUCTS, async (c) => {
-  const products = await getProducts();
+/**
+ * GET Products
+ */
+product
+  .openapi(
+    {
+      method: 'get',
+      path: ROUTES.PRODUCTS,
+      description: 'Get all products',
+      request: {
+        query: queryProductSchema,
+      },
+      responses: {
+        200: {
+          description: 'List of products',
+        },
+      },
+      tags: TAGS.PRODUCTS,
+    },
+    async (c) => {
+      try {
+        const query = c.req.query();
+        const products = await getProducts(query);
+    
+        return c.json({
+          status: true,
+          message: MESSAGE.SUCCESS.GET_PRODUCTS,
+          data: products
+        }, 200)
+      } catch (e) {
+        return c.json({
+          status: false,
+          message: e,
+        }, 404)
+      }
+    }
+  )
 
-  return c.json({
-    status: true,
-    message: MESSAGE.SUCCESS.GET_PRODUCTS,
-    data: products
-  }, 200)
-});
+/**
+ * GET Product By ID
+ */
+product
+  .openapi(
+    {
+      method: 'get',
+      path: ROUTES.PRODUCT,
+      description: 'Get product by Id',
+      request: {
+        params: paramProductByIdSchema,
+      },
+      responses: {
+        200: {
+          description: 'Product item',
+        },
+        404: {
+          description: 'Product not found',
+        },
+      },
+      tags: TAGS.PRODUCTS,
+    },
+    async (c) => {
+      const id = c.req.param('id')
+      const product = await getProductById(id)
+    
+      return c.json({
+        status: true,
+        message: MESSAGE.SUCCESS.GET_PRODUCT,
+        data: product
+      }, 200)
+    }
+  )
 
-product.get(ROUTES.PRODUCT, async (c) => {
-  const id = c.req.param('id')
-  const product = await getProductById(id)
+/**
+ * POST Add New Product
+ */
+product
+  .openapi(
+    {
+      method: 'post',
+      path: ROUTES.PRODUCTS,
+      description: 'Add product',
+      request: {
+        body: {
+          content: {
+            'application/json': {
+              schema: bodyAddProductSchema
+            },
+          }
+        }
+      },
+      responses: {
+        201: {
+          description: 'Add new product',
+        }
+      },
+      tags: TAGS.PRODUCTS,
+    },
+    async (c) => {
+      let body: Partial<Product>
+      const contentType = c.req.header('Content-Type')
+    
+      if (contentType === 'application/json') {
+        body = await c.req.json()
+      } else {
+        body = await c.req.parseBody<Partial<Product>>()
+      }
+    
+      const {name, price, image, brand_id} = body
+      const newProduct = await addProduct({name, price, image, brand_id})
+    
+      return c.json({
+        status: true,
+        message: MESSAGE.SUCCESS.ADD_PRODUCT,
+        data: newProduct
+      }, 201)
+    }
+  )
 
-  return c.json({
-    status: true,
-    message: MESSAGE.SUCCESS.GET_PRODUCT,
-    data: product
-  }, 200)
-})
+/**
+ * DELETE Product
+ */
+product
+  .openapi(
+    {
+      method: 'delete',
+      path: ROUTES.PRODUCT,
+      description: 'Delete product',
+      request: {
+       params: paramProductByIdSchema
+      },
+      responses: {
+        200: {
+          description: 'Delete product',
+        }
+      },
+      tags: TAGS.PRODUCTS,
+    },
+    async (c) => {
+      const {id} = c.req.param()
+      const deletedProduct = await deleteProductById(id)
+    
+      return c.json({
+        status: true,
+        message: MESSAGE.SUCCESS.DELETED_PRODUCT,
+        data: deletedProduct
+      })
+    }
+  )
 
-product.post(ROUTES.PRODUCTS, async (c) => {
-  let body: Partial<Product>
-  const contentType = c.req.header('Content-Type')
-
-  if (contentType === 'application/json') {
-    body = await c.req.json()
-  } else {
-    body = await c.req.parseBody<Partial<Product>>()
-  }
-
-  const {name, price, image, brand_id} = body
-  const newProduct = await addProduct({name, price, image, brand_id})
-
-  return c.json({
-    status: true,
-    message: MESSAGE.SUCCESS.ADD_PRODUCT,
-    data: newProduct
-  }, 201)
-})
-
-product.delete(ROUTES.PRODUCT, async (c) => {
-  const {id} = c.req.param()
-  const deletedProduct = await deleteProductById(id)
-
-  return c.json({
-    status: true,
-    message: MESSAGE.SUCCESS.DELETED_PRODUCT,
-    data: deletedProduct
-  })
-})
-
-product.patch(ROUTES.PRODUCT, async (c) => {
-  const id = c.req.param('id')
-  let body: Partial<Product>
-  const contentType = c.req.header('Content-Type')
-
-  if (contentType === 'application/json') {
-    body = await c.req.json()
-  } else {
-    body = await c.req.parseBody<Partial<Product>>()
-  }
-
-  const {price, name, available_stock, brand_id, image} = body
-  const newData = {
-    price, name, available_stock, brand_id, image
-  }
-  const updatedProduct = await updateProductById({id, ...newData})
-
-  return c.json({
-    status: true,
-    message: MESSAGE.SUCCESS.UPDATED_PRODUCT,
-    data: updatedProduct
-  })
-})
+/**
+ * PUT Update Product
+ */
+product
+  .openapi(
+    {
+      method: 'put',
+      path: ROUTES.PRODUCT,
+      description: 'Update product',
+      request: {
+       params: paramProductByIdSchema,
+       body: {
+        content: {
+          'application/json': {
+            schema: bodyUpdateProductSchema
+          }
+        }
+       }
+      },
+      responses: {
+        200: {
+          description: 'Update product',
+        }
+      },
+      tags: TAGS.PRODUCTS,
+    },
+    async (c) => {
+      const id = c.req.param('id')
+      let body: Partial<Product>
+      const contentType = c.req.header('Content-Type')
+    
+      if (contentType === 'application/json') {
+        body = await c.req.json()
+      } else {
+        body = await c.req.parseBody<Partial<Product>>()
+      }
+    
+      const {price, name, available_stock, brand_id, image} = body
+      const newData = {
+        price, name, available_stock, brand_id, image
+      }
+      const updatedProduct = await updateProductById({id, ...newData})
+    
+      return c.json({
+        status: true,
+        message: MESSAGE.SUCCESS.UPDATED_PRODUCT,
+        data: updatedProduct
+      })
+    }
+  )
 
 export { product };
